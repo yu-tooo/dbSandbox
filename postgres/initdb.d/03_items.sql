f835a7f6f25e5c2a5fafd7b1acbceb30fbfa00ad
yuto <teg.moritayuto@gmail.com>
2026-05-09 00:17:30 +0900

    テーブル追加

---
+++ postgres/initdb.d/03_items.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS items(
+    item_id integer,
+    year integer,
+    item_name varchar(255),
+    price_tax_ex integer,
+    price_tax_in integer
+);
---
+++ postgres/initdb.d/04_poplation.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS poplation (
+    prefecture varchar(20),
+    sex smallint,
+    pop integer
+);
